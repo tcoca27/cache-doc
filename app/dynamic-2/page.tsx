14e6cd303399de57db1700683f64c3f737053894
tudorcocaina <tudorcocaina@yahoo.com>
2024-09-19 19:38:48 +0300

    dynamic pages with client caching

---
+++ app/dynamic-2/page.tsx
@@ -0,0 +1,24 @@
+import { cookies } from "next/headers";
+import Link from "next/link";
+import React from "react";
+
+const Dynamic1Page = async () => {
+  cookies();
+  const time = await fetch("http://localhost:3000/api/dynamic", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await time.json();
+  console.log("Dynamic-2 Page Data:", data);
+
+  return (
+    <div className="flex flex-col text-center gap-10 text-lg">
+      <h1>Dynamic 2</h1>
+      <p>Response: {data.message}</p>
+      <Link href="/dynamic-1">Go To Dynamic 1</Link>
+    </div>
+  );
+};
+
+export default Dynamic1Page;
