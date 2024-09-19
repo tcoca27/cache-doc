3d3f3535c042da94742be1ee7a5cfe6e6529c6d4
tudorcocaina <tudorcocaina@yahoo.com>
2024-09-19 13:42:32 +0300

    dynamic route

--- app/api/dynamic/route.ts
+++ app/api/dynamic/route.ts
@@ -1,6 +1,8 @@
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export const GET = () => {
+  headers();
   console.log("API route called");
   const date = new Date().toTimeString();
   console.log("date in api route", date);
--- app/static/page.tsx
+++ app/static/page.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 const StaticPage = async () => {
-  const time = await fetch("http://localhost:3000/api/static-route", {
+  const time = await fetch("http://localhost:3000/api/dynamic", {
     headers: {
       "Content-Type": "application/json",
     },
   });
-  console.log("Fetched");
   const data = await time.json();
   console.log("Static Page Data:", data);
 
