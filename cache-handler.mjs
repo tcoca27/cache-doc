a5ae0888b617f858ae84812f57008f162afb6d5a
tudorcocaina <tudorcocaina@yahoo.com>
2024-09-20 11:40:32 +0300

    fixed cache

---
+++ cache-handler.mjs
@@ -0,0 +1,12 @@
+import { CacheHandler } from "@neshca/cache-handler";
+import createLruHandler from "@neshca/cache-handler/local-lru";
+
+CacheHandler.onCreation(() => {
+  const localHandler = createLruHandler();
+
+  return {
+    handlers: [localHandler],
+  };
+});
+
+export default CacheHandler;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
+import path from "node:path";
+
+const cacheHandler = path.resolve(
+  process.env.HANDLER_PATH ?? "./cache-handler.mjs"
+);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  cacheHandler:
+    process.env.NODE_ENV !== "development" ? cacheHandler : undefined,
+  cacheMaxMemorySize: 0, // disable default in-memory caching
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  experimental: {
+    instrumentationHook: true,
+    staleTimes: {
+      dynamic: 10,
+    },
+  },
+};
 
 export default nextConfig;
